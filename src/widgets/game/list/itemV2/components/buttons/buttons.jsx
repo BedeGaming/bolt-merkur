5b85a3ef1fb383acfa116325e6f5666ab632b04c
paulhess <paul.hess@bedegaming.com>
2018-09-06 14:51:12 +0100

    removed unnecessarily imported files

--- src/widgets/game/list/itemV2/components/buttons/buttons.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import Button from 'components/button.jsx';
-/**
- * @param {function} playClickHandler Function to run on click of the item
- */
-export default ({ playClickHandler }) => (
-  <Button className="btn-play" onClick={playClickHandler} text="play" />
-);
--- src/widgets/game/list/itemV2/components/details/details.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-/**
- * @param {string} displayName Display name of the game
- * @param {string} friendlyUrlPrefix URL to the game – /game/game-name
- */
-export default ({ displayName, friendlyUrlPrefix }) => (
-  <a href={friendlyUrlPrefix} className="display-name">{displayName}</a>
-);
