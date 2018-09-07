d7d958f27f02c1f77c6cfa8a48622d07dd839a39
Richard <richard.mands@bedegaming.com>
2018-09-07 16:40:49 +0100

    Styled Log In Modal

--- src/locales/en-GB.js
+++ src/locales/en-GB.js
@@ -6,7 +6,10 @@ import enGBCommon from '@bedegaming/bolt/src/locales/en-GB.js';
 const enGB = {
   'en-GB': {
     translation: {
-      footer_paragraph1: 'Welcome to Bede Casino, Bede\'s official demo site, here you will be able to utilise and play with the functionality available from Bede.<br>Our entire site is built on Bede\'s Platform API\'s with implementation and styling also coming from our internal site team.'
+      register_new_user: 'New member? <span>Create Account</span>',
+      footer_paragraph1: 'Welcome to Bede Casino, Bede\'s official demo site, here you will be able to utilise and play with the functionality available from Bede.<br>Our entire site is built on Bede\'s Platform API\'s with implementation and styling also coming from our internal site team.',
+      forgot_password: 'Forgot your password?',
+      log_in: 'Log In'
     }
   }
 };
