de9de3cd361ee6e9fad2ca925cfe63ad42f384d4
Richard <richard.mands@bedegaming.com>
2018-09-11 09:25:33 +0100

    Prevent resizing of modal on invalid input

--- src/locales/en-GB.js
+++ src/locales/en-GB.js
@@ -6,7 +6,7 @@ import enGBCommon from '@bedegaming/bolt/src/locales/en-GB.js';
 const enGB = {
   'en-GB': {
     translation: {
-      register_new_user: 'New member? <span>Create Account</span>',
+      register_new_user: 'Not a member? <span>Create Account</span>',
       footer_paragraph1: 'Welcome to Bede Casino, Bede\'s official demo site, here you will be able to utilise and play with the functionality available from Bede.<br>Our entire site is built on Bede\'s Platform API\'s with implementation and styling also coming from our internal site team.',
       forgot_password: 'Forgot your password?',
       log_in: 'Log In',
