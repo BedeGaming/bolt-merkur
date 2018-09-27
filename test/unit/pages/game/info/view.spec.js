03e9a6cd2d397b8320effc721351f86f587fba70
Michael K. Clements <michaelkc@me.com>
2018-09-27 09:09:41 +0100

    Trying to fix teamcity failing

--- test/unit/pages/game/info/view.spec.js
+++ test/unit/pages/game/info/view.spec.js
@@ -8,9 +8,9 @@ describe('game info view', function () {
     const View = proxyquire('pages/game/info/view.js', {
       '@bedegaming/bolt/src/widgets/game/banner/view.js': this.BannerView,
       '@bedegaming/bolt/src/widgets/game/info/view.js': this.InfoView,
-      '@bedegaming/bolt/src/widgets/game/related/view.js': this.RelatedView,
+      '@bedegaming/bolt/src/widgets/game/overview/view.js': this.GameOverview,
       '@bedegaming/bolt/src/widgets/game/play-buttons/view.js': this.PlayButtons,
-      '@bedegaming/bolt/src/widgets/game/overview/view.js': this.GameOverview
+      '@bedegaming/bolt/src/widgets/game/related/view.js': this.RelatedView
     });
     this.view = new View();
   });
@@ -18,10 +18,10 @@ describe('game info view', function () {
   describe('regions', function () {
     it('should create the regions', function () {
       expect(this.view.regions).to.eql({
-        overview: '.game-overview-region',
-        playButtons: '.play-buttons-region',
         banner: 'game-banner',
         info: 'game-info',
+        overview: '.game-overview-region',
+        playButtons: '.play-buttons-region',
         related: 'game-related'
       });
     });
