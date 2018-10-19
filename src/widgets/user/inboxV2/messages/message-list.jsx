822b7421797009c663d0803fac478183f5da7531
paulhess <paul.hess@bedegaming.com>
2018-10-19 14:07:29 +0100

    fixed linting errors

--- src/widgets/user/inboxV2/messages/message-list.jsx
+++ src/widgets/user/inboxV2/messages/message-list.jsx
@@ -1,32 +1,28 @@
 import React from 'react';
 import { Translator } from 'orchestra';
-import Message from './message.jsx';
+import Message from 'widgets/user/inboxV2/messages/message.jsx';
 
-export default function ({ state: props, actions }) {
+export default function({ state: props, actions }) {
   const { messages, pending, errors } = props;
   const emptyInbox = Translator.translate('inbox_empty');
   const errorMessage = Translator.translate('messages_fetch_failed');
-  const messageList = messages.map(message => <Message key={message.id} actions={actions} {...message}/>);
+  const messageList = messages.map(message => (
+    <Message key={message.id} actions={actions} {...message} />
+  ));
   const messagesErrors = errors.filter(msg => msg.messages);
   let listContent = null;
 
   const isPending = pending.messages;
 
   if (isPending && !messageList.length) {
-    listContent = (
-      <div className="spinner"></div>
-    );
+    listContent = <div className="spinner" />;
   } else {
     if (messagesErrors.length) {
-      listContent = <p>{ errorMessage }</p>;
+      listContent = <p>{errorMessage}</p>;
     } else {
-      listContent = messages.length ? messageList : (<p>{ emptyInbox }</p>);
+      listContent = messages.length ? messageList : <p>{emptyInbox}</p>;
     }
   }
 
-  return (
-    <div className="inbox-list">
-      { listContent }
-    </div>
-  );
+  return <div className="inbox-list">{listContent}</div>;
 }
