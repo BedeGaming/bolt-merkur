a76475fcc60e2ab1dc3651db28d16404fef932fe
paulhess <paul.hess@bedegaming.com>
2018-11-01 15:50:03 +0000

    tidied up some awful js

--- src/widgets/bank/add-card/view.js
+++ src/widgets/bank/add-card/view.js
@@ -4,21 +4,17 @@ import AddCardView from '@bedegaming/bolt/src/widgets/bank/add-card/view.js';
 export default AddCardView.extend({
   /* istanbul ignore next */
   events: _.extend({}, AddCardView.prototype.events, {
-    'keyup #deposit-input-expiry-date': 'checkIfHasValue',
-    'blur #deposit-input-card-number': 'checkIfHasValue'
+    'keyup .expiry-date': 'checkIfHasValue',
+    'blur .credit-card': 'checkIfHasValue'
   }),
 
   checkIfHasValue() {
-    if (document.getElementById('deposit-input-expiry-date').value) {
-      document
-        .getElementById('deposit-input-expiry-date')
-        .classList.add('has-value');
+    if (document.querySelector('.expiry-date').value) {
+      document.querySelector('.expiry-date').classList.add('has-value');
     }
 
-    if (document.getElementById('deposit-input-card-number').value) {
-      document
-        .getElementById('deposit-input-card-number')
-        .classList.add('has-value');
+    if (document.querySelector('.credit-card').value) {
+      document.querySelector('.credit-card').classList.add('has-value');
     }
   }
 });
